b432041228f45344301114549e0368e80d256912
Shubham Puri <41786574+shubham7775881631@users.noreply.github.com>
2021-11-28 18:45:50 +0530

    Delete DEMO_1/Infy-EmployeeFinance directory

--- DEMO_1/Infy-EmployeeFinance/src/main/java/com/infy/employee/finance/repository/FinanceRepository.java
+++
@@ -1,9 +0,0 @@
-package com.infy.employee.finance.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.infy.employee.finance.entity.Finance;
-
-public interface FinanceRepository extends JpaRepository<Finance, Integer> {
-
-}
