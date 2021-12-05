8808434585c1cda0188f1b22b1358210a3b4ca57
Shubham Puri <41786574+shubham7775881631@users.noreply.github.com>
2021-12-05 21:02:47 +0530

    Delete Infy-Employee directory

--- Infy-Employee/src/main/java/com/infy/employee/repository/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.infy.employee.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.infy.employee.entity.Employee;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
-
-}
