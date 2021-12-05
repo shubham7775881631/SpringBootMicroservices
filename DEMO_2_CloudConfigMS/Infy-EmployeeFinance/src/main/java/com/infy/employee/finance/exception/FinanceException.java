f695e13860aaee4ef9489415bd50835d5ee349e4
Shubham Puri <41786574+shubham7775881631@users.noreply.github.com>
2021-12-05 17:00:20 +0530

    Add files via upload

---
+++ DEMO_2_CloudConfigMS/Infy-EmployeeFinance/src/main/java/com/infy/employee/finance/exception/FinanceException.java
@@ -0,0 +1,12 @@
+package com.infy.employee.finance.exception;
+
+public class FinanceException extends Exception{
+	
+	public static final long serialVersionUID = 1l;
+	
+	public FinanceException(String message) {
+		
+		super(message);
+	}
+
+}
