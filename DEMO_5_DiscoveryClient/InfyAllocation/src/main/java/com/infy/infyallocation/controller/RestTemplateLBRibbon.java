98d3d6b7df597a80a51ff36599434b98571de875
Shubham Puri <41786574+shubham7775881631@users.noreply.github.com>
2022-01-25 09:38:43 +0530

    Add files via upload

---
+++ DEMO_5_DiscoveryClient/InfyAllocation/src/main/java/com/infy/infyallocation/controller/RestTemplateLBRibbon.java
@@ -0,0 +1,17 @@
+package com.infy.infyallocation.controller;
+
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class RestTemplateLBRibbon {
+	
+	@Bean @LoadBalanced
+	public RestTemplate getRestTemplate()
+	{
+		return new RestTemplate();
+	}
+
+}
