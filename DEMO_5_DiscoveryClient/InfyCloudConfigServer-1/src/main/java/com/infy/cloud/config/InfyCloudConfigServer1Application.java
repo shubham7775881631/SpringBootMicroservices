3228da9acb33728ee7db55c819ebad84630e9d13
Shubham Puri <41786574+shubham7775881631@users.noreply.github.com>
2022-01-25 09:42:08 +0530

    Add files via upload

---
+++ DEMO_5_DiscoveryClient/InfyCloudConfigServer-1/src/main/java/com/infy/cloud/config/InfyCloudConfigServer1Application.java
@@ -0,0 +1,15 @@
+package com.infy.cloud.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class InfyCloudConfigServer1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfyCloudConfigServer1Application.class, args);
+	}
+
+}
