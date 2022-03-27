c03861c4a190d2d140463825891560cab05c6c65
Shubham Puri <41786574+shubham7775881631@users.noreply.github.com>
2022-03-27 12:42:34 +0530

    Add files via upload

---
+++ DEMO_6_EurekaWithHystrixResiliency/Infy-Employee/src/main/java/com/infy/employee/DemoApplication.java
@@ -0,0 +1,25 @@
+package com.infy.employee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.web.client.RestTemplate;
+
+
+@PropertySource("classpath:validationMessages.properties")
+@SpringBootApplication
+@EnableEurekaClient
+@EnableCircuitBreaker
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+	
+	
+	
+	
+}
