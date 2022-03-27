f4316706babfca7040147b7e51338fb171a3da66
Shubham Puri <41786574+shubham7775881631@users.noreply.github.com>
2022-03-27 12:52:21 +0530

    Add files via upload

---
+++ DEMO_7_Eureka-Hystrix-SyncComm/InfyDemo-1/src/test/java/com/infy/employee/finance/InfyDemo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infy.employee.finance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfyDemo1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
