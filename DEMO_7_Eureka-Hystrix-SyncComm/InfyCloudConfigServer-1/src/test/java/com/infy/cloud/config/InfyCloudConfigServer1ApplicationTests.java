a2cf6a804ce83405d4fa1fa7fba647a66d0d5b9c
Shubham Puri <41786574+shubham7775881631@users.noreply.github.com>
2022-03-27 12:51:20 +0530

    Add files via upload

---
+++ DEMO_7_Eureka-Hystrix-SyncComm/InfyCloudConfigServer-1/src/test/java/com/infy/cloud/config/InfyCloudConfigServer1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infy.cloud.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfyCloudConfigServer1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
