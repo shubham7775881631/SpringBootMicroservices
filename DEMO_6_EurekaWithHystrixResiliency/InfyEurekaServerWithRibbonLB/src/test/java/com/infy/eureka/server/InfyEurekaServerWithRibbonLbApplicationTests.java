21f221ff5badf9ccc6e5ea421195a8228d890fc7
Shubham Puri <41786574+shubham7775881631@users.noreply.github.com>
2022-03-27 12:44:31 +0530

    Add files via upload

---
+++ DEMO_6_EurekaWithHystrixResiliency/InfyEurekaServerWithRibbonLB/src/test/java/com/infy/eureka/server/InfyEurekaServerWithRibbonLbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infy.eureka.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfyEurekaServerWithRibbonLbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
